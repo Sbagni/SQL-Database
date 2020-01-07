db21bb7264458c0a6a06c83e3073965f074fda13
Gaurav Agnihotri <sumatibhala@gmail.com>
2020-01-07 10:48:19 -0500

    change the query from emp to e

--- Query_sql.sql
+++ Query_sql.sql
@@ -1,6 +1,6 @@
 --List the following details of each employee: employee number, last name, first name, gender, and salary.
 
-SELECT emp
+SELECT e
 .last_name, e.first_name, e.gender,s.salary
 FROM salaries AS s
 INNER JOIN employees AS e ON
