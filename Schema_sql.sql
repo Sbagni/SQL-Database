ebedf4c3720b3aa0388459c1e68a929701476dc4
Gaurav Agnihotri <sumatibhala@gmail.com>
2020-01-07 10:50:20 -0500

    update emp_no

--- Schema_sql.sql
+++ Schema_sql.sql
@@ -1,5 +1,5 @@
 CREATE TABLE employees (
-    emp_not      INT             NOT NULL,
+    emp_no      INT             NOT NULL,
     birth_date  DATE            NOT NULL,
     first_name  VARCHAR(14)     NOT NULL,
     last_name   VARCHAR(16)     NOT NULL,
